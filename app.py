e7cb0db8029567f9b817306de9fef2cc18c63790
GlebMukhievPI <136423005+progibator22@users.noreply.github.com>
2025-11-22 16:36:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    student_name = os.getenv('STUDENT_NAME', 'Девопсов Олег')
+    student_name = os.getenv('STUDENT_NAME', 'Мухиев Глеб')
     return render_template('index.html', student_name=student_name)
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8081))
+    port = int(os.getenv('PORT', 8034))
     app.run(host='0.0.0.0', port=port)
